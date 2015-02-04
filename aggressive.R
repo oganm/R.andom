33126aab80899f6da4478ad07011bde155f66373
OganM <ogan.mancarcii@gmail.com>
2015-02-03 20:16:10 -0800

    aggresive made more aggressive

--- aggressive.R
+++ aggressive.R
@@ -1,11 +1,18 @@
 options("error" = function(){
-    a=floor(runif(1,1,6))
+    a=floor(runif(1,1,14))
     switch(a,
-           {print('You failed again Lila')},
+           {print('You failed again')},
            {print('When will you learn?')},
            {print('Your code is bad and you should feel bad')},
            {print('Incompetence at the highest echelons of power!')},
-           {print("Just give up")})
-    print('You fucked it up moron')
+           {print("Just give up")},
+           {print("Don't quit your day job")},
+           {print("Why are you even trying?")},
+           {print("It's not R. It's you")},
+           {print("You fucked it up")},
+           {print("Go back to BASIC")},
+           {print("What were you thinking!")},
+           {print("This is bullshit!")},
+           {print("I hope you are not getting paid for this")})
     .rs.recordTraceback(TRUE)
 })
