d542db21645f82a956e820210ee543de0bb9d324
Ogan Mancarci <ogan.mancarcii@gmail.com>
2016-03-17 22:55:17 -0700

    animate objects crit implemented

--- animateObjects.R
+++ animateObjects.R
@@ -1,15 +1,23 @@
 animate = function(AC, bonus = 8, count = 10, advantage = F, dice = 4 , damageBonus = 4){
-  out = sapply(1:count, function(x){
-    out = floor(runif(1,1,21))
-    if (advantage == T){
-      out2 = floor(runif(1,1,21))
-      out = max(out,out2)
-    }
-    return(out)
+    out = sapply(1:count, function(x){
+        out = sample(1:20,size = 1)
+        if (advantage == T){
+            out2 = sample(1:20,size = 1)
+            out = max(out,out2)
+        }
+        return(out)
     })
-  
-  sum(sapply(1:sum(out + bonus - AC >= 0), function(x){
-    floor(runif(1,1,dice+1))+ damageBonus
-  }))
-  
+    
+    sum(
+        sapply(out,function(x){
+            if(x == 20){
+                return(sum(sample(1:dice,2)) + damageBonus)
+            } else{
+                if (x + bonus - AC >= 0){
+                    return(sample(1:dice,1) + damageBonus)
+                } else {
+                    return(0)
+                }
+            }
+        }))
 }
