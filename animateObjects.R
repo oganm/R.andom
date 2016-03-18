ac2fb43fd2fbc1a1292e3b0263f37a6b3be52fe9
Ogan Mancarci <ogan.mancarcii@gmail.com>
2016-03-17 23:10:43 -0700

    different die types

--- animateObjects.R
+++ animateObjects.R
@@ -1,4 +1,12 @@
-animate = function(AC, bonus = 8, count = 10, advantage = 'N', dice = 4 , damageBonus = 4){
+# AC: armor class
+# bonus: attack bonus of the group
+# count: number of entities in the group
+# advantage: N = normal. A = with advantage, D = with disadvantage
+# dice = number and type of die
+# damageBonus = bonus to damage
+# default settings are for animating 10 tiny objects but should work with any mob of identical creatures
+
+animate = function(AC, bonus = 8, count = 10, advantage = 'N', dice = '1d4' , damageBonus = 4){
     out = sapply(1:count, function(x){
         out = sample(1:20,size = 1)
         if (advantage == 'A'){
@@ -11,13 +19,16 @@ animate = function(AC, bonus = 8, count = 10, advantage = 'N', dice = 4 , damage
         return(out)
     })
     
+    diceCount = as.integer(regmatches(dice,regexpr(".*?(?=d)",dice,perl = T)))
+    diceSide = as.integer(regmatches(dice,regexpr("(?<=d).*",dice,perl = T)))
+    
     sum(
         sapply(out,function(x){
             if(x == 20){
-                return(sum(sample(1:dice,2)) + damageBonus)
+                return(sum(sample(1:diceSide,diceCount*2)) + damageBonus)
             } else{
                 if (x + bonus - AC >= 0){
-                    return(sample(1:dice,1) + damageBonus)
+                    return(sample(1:diceSide,diceCount) + damageBonus)
                 } else {
                     return(0)
                 }
